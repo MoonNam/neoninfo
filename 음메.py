970f56b0e23c5a1dd1854d8e2c7f3fb0fb3ffccb
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-02-29 21:40:31 +0900

    Add files via upload

---
+++ 음메.py
@@ -0,0 +1,33 @@
+import discord
+import datetime
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    game = discord.Game("음메")
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print("------------------")
+
+@client.event
+async def on_message(message, value=None):
+    if message.content.startswith("/음메"):
+        date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+        embed = discord.Embed(color=0xff0000)
+        embed.add_field(name="서버", value="NEON", inline=True)
+        embed.add_field(name="사용자", value=message.author.name, inline=True)
+        embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일",
+                        inline=True)
+        embed.add_field(name="고유번호/닉네임/직업", value=message.author.display_name, inline=True)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="제작자", value="음메#7491", inline=True)
+        embed.set_image(url="https://o.remove.bg/uploads/a3011d27-6843-495a-85ad-fd723ef974f9/prop_ron_drop_sign_.png")
+        await message.channel.send(embed=embed)
+    if message.content.startswith("/제작자"):
+        await message.channel.send("제작자 : 음메#7491")
+
+client.run("NjgzMjgyODU5ODk2NzMzNzQz.XlpS1w.0SKbtQHbd6PolsAzNwdlztM2tIY")
