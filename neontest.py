c52a1e298c41243b5bd084e2bb469a0b0df11515
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-03-01 17:48:28 +0900

    Update neontest.py

--- neontest.py
+++ neontest.py
@@ -11,7 +11,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------------------")
-    game = discord.Game("음메대신")
+    game = discord.Game("음메 대신")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
