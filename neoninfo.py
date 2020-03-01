05c55f00b604d8ac176f7d74c7b3468f2bf40b5a
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-03-01 17:16:10 +0900

    Update neoninfo.py

--- neoninfo.py
+++ neoninfo.py
@@ -15,7 +15,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message, value=None):
-    if message.content.startswith("/"):
+    if message.content.startswith("/음메"):
         date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
         embed = discord.Embed(color=0xff0000)
         embed.add_field(name="서버", value="NEON", inline=True)
@@ -26,6 +26,7 @@ async def on_message(message, value=None):
         embed.add_field(name="제작자", value="음메#7491", inline=True)
         embed.set_image(url="https://o.remove.bg/uploads/a3011d27-6843-495a-85ad-fd723ef974f9/prop_ron_drop_sign_.png")
         await message.channel.send(embed=embed)
+        
     if message.content.startswith("/제작자"):
         await message.channel.send("제작자 : 음메#7491")
     if message.content.startswith("/안녕"):
@@ -61,6 +62,7 @@ async def on_message(message, value=None):
             emb = discord.Embed(title='가위바위보', color=0xff0000)
             emb.add_field(name='승부결과!!', value='음메 :raised_hand: 당신 :fist: 봇 승리!')
             await message.channel.send(content=None, embed=emb)
+            
     if message.content.startswith("/가위바위보 보"):
         rsp = "123"
         rsp1 = random.choice(rsp)
@@ -75,6 +77,7 @@ async def on_message(message, value=None):
         if rsp1 == "3":
             emb = discord.Embed(title='가위바위보', color=0xfff000)
             emb.add_field(name='승부결과!!', value='음메 :raised_hand: 당신 :raised_hand: 무승부!')
-            await message.channel.send(content=None, embed=emb)        
+            await message.channel.send(content=None, embed=emb)   
+            
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
